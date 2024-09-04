f7292ccbb87282f6295459ab8b0f7f79f45739d0
j-Abban <justice.abban@amalitech.org>
2024-09-04 11:59:03 +0000

    created method to withdraw

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -13,6 +13,14 @@ public class SavingsAccount {
     }
 // Method to deposit money to Account
     public void deposit(int amountToDeposit){
-
+    balance = balance + amountToDeposit;
+    System.out.println("You just deposited " + amountToDeposit);
     }
+// Method to Withdraw money from the Savings Account
+    public int withdraw(int amountToWithdraw){
+        balance = balance - amountToWithdraw;
+        System.out.println("You just withdrew " + amountToWithdraw);
+        return amountToWithdraw;
+    }
+
 }
