8bb9f793b5c318d360c3baa085d860646c921e2f
j-Abban <justice.abban@amalitech.org>
2024-09-04 14:22:55 +0000

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // create Savings account with initial balance of 4200
+        SavingsAccount savings = new SavingsAccount(4200);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // check balance
+        savings.checkBalance();
+
+        // Withdrawing
+        savings.withdraw(200);
+
+        // Check balance
+        savings.checkBalance();
+
+        // Deposit to my account
+        savings.deposit(800);
+
+        // Check balance
+        savings.checkBalance();
+
     }
 }
--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,6 +1,6 @@
 public class SavingsAccount {
     int balance;
-    String name = "Justice Abban";
+    String name = "Justice";
 
     // SavingsAccount constructor method
     public SavingsAccount(int initialBalance){
@@ -9,7 +9,8 @@ public class SavingsAccount {
 // Method to check balance
     public void checkBalance(){
         System.out.println("Hello " + name);
-        System.out.println("Your balance is " + balance);
+        System.out.println("Your balance is " + balance + ".");
+        System.out.println("Thank you for banking with us.");
     }
 // Method to deposit money to Account
     public void deposit(int amountToDeposit){
