50bb56ef8bdbdd0c5cd2fa1759ffa1ea3d7620ab
j-Abban <justice.abban@amalitech.org>
2024-09-04 15:22:49 +0000

    Print the string representation of the SavingsAccount object

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,22 @@ public class Main {
         // create Savings account with initial balance of 4200
         SavingsAccount savings = new SavingsAccount(4200);
 
-        // check balance
+        // check balance using the check balance method
         savings.checkBalance();
 
         // Withdrawing
         savings.withdraw(200);
 
-        // Check balance
+        // Check balance again
         savings.checkBalance();
 
-        // Deposit to my account
+        // Deposit
         savings.deposit(800);
 
-        // Check balance
+        // Check balance again
         savings.checkBalance();
+
+        // Print the string representation of the SavingsAccount object
+        System.out.println(savings.toString());
     }
 }
