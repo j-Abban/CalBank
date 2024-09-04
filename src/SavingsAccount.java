5a5ff34178cb2aa6dc82b17b530effd30cb19ed8
j-Abban <justice.abban@amalitech.org>
2024-09-04 11:43:21 +0000

    Java mini project

---
+++ src/SavingsAccount.java
@@ -0,0 +1,18 @@
+public class SavingsAccount {
+    int balance;
+    String name = "Justice Abban";
+
+    // SavingsAccount constructor method
+    public SavingsAccount(int initialBalance){
+        balance = initialBalance;
+    }
+// Method to check balance
+    public void checkBalance(){
+        System.out.println("Hello " + name);
+        System.out.println("Your balance is " + balance);
+    }
+// Method to deposit money to Account
+    public void deposit(int amountToDeposit){
+
+    }
+}
